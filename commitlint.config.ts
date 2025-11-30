bfb9497350145adcb131e4cc8d43fdd5b4a0b4a1
Janusz Dziurzyński <janusz@forserial.org>
2025-11-30 02:52:32 +0000

    chore(ci): maybe fix semantic release

---
+++ commitlint.config.ts
@@ -0,0 +1,6 @@
+import type { UserConfig } from '@commitlint/types';
+
+export default {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [(message) => /^\s*chore\(\s*release\s*\)/.test(message)],
+} satisfies UserConfig;
